a6dcd60f71667e62670ff4aa783349d0b76582c9
Milad Moulayi <m.moulayi123@gmail.com>
2023-04-03 14:35:58 -0700

    Refactored RetiredForager

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -4,11 +4,10 @@ var RetiredForagerBee = function() {
   this.color = 'grey';
   this.job = 'gamble';
   this.canFly = false;
-  this.treasureChest = [];
 };
 
 RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
 RetiredForagerBee.prototype.constructor = RetiredForagerBee;
 
 RetiredForagerBee.prototype.forage = function () { return 'I am too old, let me play cards instead' };
-RetiredForagerBee.prototype.gamble = function () { this.treasureChest.push('treasure'); }
+RetiredForagerBee.prototype.gamble = function () { ForagerBee.prototype.forage.call(this, 'treasure'); }
